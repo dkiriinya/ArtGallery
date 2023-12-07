dbfbd52c60a44f2d4e1a9f85241f656c9ab51d55
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-07 09:58:39 +0300

    pulled items from Josephs repository

---
+++ js/loops.js
@@ -0,0 +1,22 @@
+// for (let i = 1; i > -5;i--){
+//     console.log(i)
+// }
+// let i = 1
+// while(i<6){
+//     console.log(i)
+//     i++
+// }
+// let x = 5
+// let y = 2
+
+// // power
+// console.log(x**=y)
+// // remainder
+// console.log(x%=y)
+
+ let e = 1
+
+do {
+    console.log(e);
+    e++;
+} while(e<6);

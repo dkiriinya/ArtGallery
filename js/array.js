1a03e18a896e2d8499b12d71154f171129857422
josephbill <josephbill00@gmail.com>
2023-12-20 09:21:11 +0300

    changes in array.js and functions.js

--- js/array.js
+++ js/array.js
@@ -1,5 +1,5 @@
 // Data Structure : formats of saving a collection of data 
-// linear structures : elements are accessed sequencially
+// linear structures : elements are accessed sequencially/sorted
 // non-linear structures : elements are accessed using keys
 // phase 0: Arrays and Objects 
 // Array : used to store multiple values in a single variable
@@ -14,9 +14,42 @@ fruits[1] = "Pear"
 console.log(fruits)
 //add elements in my array 
 fruits.push("grapes")
-console.log(fruits)
+console.log("pushed new fruit",fruits)
+//pop 
+const removedFruit = fruits.shift()
+console.log("shifted fruit ", removedFruit)
+console.log("value of array before splice ", fruits)
+fruits.splice(3,3,'cherry')
+console.log("value of array after splice ", fruits)
+
+let fruit = ["mango","pineapple"]
+let combinedFruits = fruits.concat(fruit)
+console.log(combinedFruits)
+
+let slicedFruits = combinedFruits.slice(1,3)
+console.log("sliced fruits ", slicedFruits)
 
 // trasverse/loop/enumerate : arrays : for loop , forEach , for..of , for...in
 for(let i=0; i < fruits.length; i++){
     console.log(fruits[i])
 }
+
+// Define 
+/*
+Arrays are ordered collections of values. Each value is assigned an index which allows access to the value. 0....length of the array-1
+Homogeneous : typically used to store homogeneous data types , meaning all elements in the array are usually of the same data type 
+["Joseph", {} , true, 6] : [6,7,7,4,5,3,5]
+Array Methods : arrays have a variety of in built methods (manipulation)
+- push : adds elements () end of the array , return a new length arrays. 
+- pop : removes the last element of the array, return a new length of the array , 
+return that element
+- shift : removes the first element of the array, returns that element 
+- unshift : adds elements to the beginning of the array, returns a new lenght of the array
+- splice : changes the content of an array by removing , adding or replacing elements at 
+a specified index 
+- concat : combines two or more array and returns a new array without modifying the original 
+arrays 
+- slice : returns a shallow copy of a portion of an array into a new array object , 
+specification is done from start to end (end not included)
+................................
+*/
--- js/functions.js
+++ js/functions.js
@@ -56,11 +56,33 @@ add(5,5,display)
 console.log(x)
 /*
 {
-
+this 
 }
 
 function name(){
-
+    let x = 5
+    this.x
 }
 
+Lexical Scoping 
+Closures : a function that remembers its outer scope, forming a new chain 
+
+Execution Context : components include : variable environment , scope chain , this binding 
+How does a function work ? 
+1. Creation phase : declaration and initialization 
+2. Execution phase : 1. Code is executed line by line 2. Variables are assigned values as code runs 3. Function creates new execution context
+*/
+
+// recap on array and objects 
+// Bisection search : finding an element within a list by repeatedly dividing the search interval in half 
+// concepts : divide and conquer [1,2,3,.....10] -> [1...5]
+// More algorithms. 
+
+/*
+1. Build a computer guessing game 
+2. This is how it works 
+a. The user will think of a number within a range [1....10]
+b. The program should output an initial number e.g 3
+c. The user should give a response to the number indicating whether program's guess is high(h) , low(l) , or correct(c)
+concepts (arrays, loops , variables, bisection search)
 */
